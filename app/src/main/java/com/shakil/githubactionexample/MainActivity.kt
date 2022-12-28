d6744625fa3c304da5e40b9ceb5d9de22808fe26
Shakil Karim <shakil807g@gmail.com>
2022-12-28 20:35:04 +0500

    Revert "some new commit"
    This reverts commit 38ce71df752a11a0e6c52c10a14cee359ac0fcbc.

--- app/src/main/java/com/shakil/githubactionexample/MainActivity.kt
+++ app/src/main/java/com/shakil/githubactionexample/MainActivity.kt
@@ -3,7 +3,6 @@ package com.shakil.githubactionexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,10 +16,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.relocation.BringIntoViewRequester
-import androidx.compose.foundation.relocation.bringIntoViewRequester
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -33,22 +28,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shakil.githubactionexample.ui.theme.GithubActionExampleTheme
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -68,17 +59,15 @@ fun Greeting() {
         val list by remember { mutableStateOf((1..10).toList()) }
         val map = rememberMap(list.size)
         val state = rememberLazyListState()
-        var focusIndex by remember { mutableStateOf(0) }
         val scope = rememberCoroutineScope()
-        ToolBarButton(focusIndex, state, list.size)
+        ToolBarButton()
         LazyColumn(
             state = state,
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f), content = {
-                itemsIndexed(list) { index, _ ->
-                    Content(state, map, index, list) { fIndex ->
-                        focusIndex = fIndex
+                .weight(1f),content = {
+                itemsIndexed(list) { index,_ ->
+                    Content(state,map, index, list) { fIndex ->
                         scope.launch {
                             state.scrollToItem(fIndex)
                         }
@@ -91,47 +80,24 @@ fun Greeting() {
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-private fun ToolBarButton(focusIndex: Int, state: LazyListState, size: Int) {
-    val scope = rememberCoroutineScope()
-    val focusManager = LocalFocusManager.current
-
+private fun ToolBarButton() {
     Row(Modifier.fillMaxWidth()) {
+        val focusManager = LocalFocusManager.current
         Button(onClick = {
-            scope.launch {
-                val newIndex = focusIndex - 1
-                if (newIndex < 0) {
-                    state.scrollToItem(size - 1)
-                } else {
-                    state.scrollToItem(newIndex)
-                }
-                delay(200)
-                focusManager.moveFocus(FocusDirection.Up)
-            }
-
+            focusManager.moveFocus(FocusDirection.Up)
         }) {
             Text("UPS")
 
         }
         Button(onClick = {
-            scope.launch {
-                val newIndex = focusIndex + 1
-                if (newIndex == size) {
-                    state.scrollToItem(0)
-                } else {
-                    state.scrollToItem(newIndex)
-                }
-                delay(200)
-                focusManager.moveFocus(FocusDirection.Down)
-            }
+            focusManager.moveFocus(FocusDirection.Down)
         }) {
             Text("DOWN")
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun Content(
     state: LazyListState,
@@ -141,56 +107,61 @@ private fun Content(
     onFocusChange: (Int) -> Unit
 ) {
     val scope = rememberCoroutineScope()
-    val bringIntoViewRequester = remember { BringIntoViewRequester() }
+    val firstFocus = remember(index, map) { getFocusRequester(map, index) }
+    val secoundFocus = remember(index,list, map) { getFocusRequester(map, list.size + index) }
+
+
     Row(
         Modifier
             .fillMaxWidth()
-            .bringIntoViewRequester(bringIntoViewRequester),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
+            .height(200.dp), horizontalArrangement = Arrangement.SpaceBetween) {
         var text1 by remember { mutableStateOf(TextFieldValue("${index}")) }
         var text2 by remember { mutableStateOf(TextFieldValue("${list.size + index}")) }
 
         TextField(modifier = Modifier
             .weight(1f)
-            .height(200.dp)
-            .focusRequester(getFocusRequester(map, index))
-            .focusProperties {
-                down = if (index == list.lastIndex) getFocusRequester(map, 0)
-                else getFocusRequester(map, index + 1)
-                up = if (index == 0) getFocusRequester(map, list.lastIndex)
-                else getFocusRequester(map, index - 1)
-            }
-            .onFocusEvent { focusState ->
-                if (focusState.isFocused) {
-                    onFocusChange(index)
-                }
-            },
-            value = text1, onValueChange = { newText ->
-                text1 = newText
-            })
+//            .focusRequester(firstFocus)
+//            .focusProperties {
+//                down =
+//                    if (index == list.lastIndex) getFocusRequester(map, 0) else getFocusRequester(
+//                        map,
+//                        index + 1
+//                    )
+//                up = if (index == 0) getFocusRequester(map, list.lastIndex) else getFocusRequester(
+//                    map,
+//                    index - 1
+//                )
+//            }
+//            .onFocusChanged {
+//                if (it.hasFocus) {
+//                    onFocusChange(index)
+//                }
+//            }
+            , value = text1, onValueChange = { newText ->
+            text1 = newText
+        })
         Spacer(modifier = Modifier.width(10.dp))
         TextField(modifier = Modifier
             .weight(1f)
-            .focusRequester(getFocusRequester(map, list.size + index))
-            .focusProperties {
-                down = if (index == list.lastIndex) getFocusRequester(
-                    map,
-                    list.size
-                ) else getFocusRequester(map, list.size + index + 1)
-                up = if (index == 0) getFocusRequester(
-                    map,
-                    list.size + list.lastIndex
-                ) else getFocusRequester(map, list.size + index - 1)
-            }
-            .onFocusEvent { focusState ->
-                if (focusState.isFocused) {
-                    onFocusChange(index)
-                }
-            },
-            value = text2, onValueChange = { newText ->
-                text2 = newText
-            })
+//            .focusRequester(secoundFocus)
+//            .focusProperties {
+//                down = if (index == list.lastIndex) getFocusRequester(
+//                    map,
+//                    list.size
+//                ) else getFocusRequester(map, list.size + index + 1)
+//                up = if (index == 0) getFocusRequester(
+//                    map,
+//                    list.size + list.lastIndex
+//                ) else getFocusRequester(map, list.size + index - 1)
+//            }
+//            .onFocusChanged {
+//                if (it.hasFocus) {
+//                    onFocusChange(index)
+//                }
+//            }
+            , value = text2, onValueChange = { newText ->
+            text2 = newText
+        })
     }
 }
 
@@ -201,7 +172,7 @@ private fun getFocusRequester(
 
 
 @Composable
-fun rememberMap(size: Int): Map<Int, FocusRequester> {
+fun rememberMap(size: Int): Map<Int,FocusRequester> {
     return remember(size) {
         (0 until (size * 2)).associateWith { FocusRequester() }
     }
